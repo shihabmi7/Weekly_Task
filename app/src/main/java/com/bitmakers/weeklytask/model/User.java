57f2cfc58ddaf535da57da266cffe8336d1fdb5e
shihabmi7 <shihab.mi7@gmail.com>
2016-05-12 16:21:45 +0600

    // add 3 layout ; Project , Goal , Task Creation

---
+++ app/src/main/java/com/bitmakers/weeklytask/model/User.java
@@ -0,0 +1,13 @@
+package com.bitmakers.weeklytask.model;
+
+/**
+ * Created by ASUS on 5/12/2016.
+ */
+public class User {
+
+    public String user_name;
+    public String email;
+    public String password;
+
+
+}
